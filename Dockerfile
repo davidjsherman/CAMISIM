d449d615171d47bc7426354bb7ed81366d105e41
sherman <david.sherman@inria.fr>
2020-12-11 18:01:29 +0100

    Dockerfile and entry point
    - CAMISIM environment variable points to install directory
    - Don't try to fix inconsistency between CAMISIM and ete2 taxonomies

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,18 @@ RUN apt install -y python2.7 python-pip perl
 RUN perl -MCPAN -e 'install XML::Simple'
 ADD requirements.txt /requirements.txt
 RUN cat requirements.txt | xargs -n 1 pip install
-ADD *.py /usr/local/bin/
-ADD scripts /usr/local/bin/scripts
-ADD tools /usr/local/bin/tools
-ADD defaults /usr/local/bin/defaults
+
+ENV CAMISIM=/usr/local/bin
+
+ADD *.py $CAMISIM/
+ADD scripts $CAMISIM/scripts
+ADD tools $CAMISIM/tools
+ADD defaults $CAMISIM/defaults
 ADD ete2.patch /
 RUN patch $(python -c "import ete2 as _; print(_.__path__[0])")/ncbi_taxonomy/ncbiquery.py ete2.patch
-ADD docker_entrypoint.sh /usr/local/bin/entrypoint.sh
-ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
+
+ADD docker_entrypoint.sh $CAMISIM/entrypoint.sh
+ENTRYPOINT ["$CAMISIM/entrypoint.sh"]
 
 RUN useradd -d /tmp camisim
 USER camisim
-
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -8,12 +8,10 @@
 # from its install directory, rather than a writeable working
 # directory; and by ete2, that it can write both in its invocation
 # directory and in $HOME.
-# Also works around an inconsisteny, where CAMISIM is provided an NCBI
-# taxonomy file, but ete2 downloads a new one from the NCBI anyway.
 
 # CAMISIM expects to be invoked from its install directory.
 # In an ideal world this variable is set by the container image.
-CASMISIM=${CAMISIM:-/usr/local/bin}
+CASISIM=${CAMISIM:-/usr/local/bin}
 
 # $HOME must be writeable, even if the container user is unprivileged.
 # Note as per [4.2](https://www.commonwl.org/v1.1/CommandLineTool.html#Runtime_environment)
@@ -26,26 +24,7 @@ TAXDUMP=$HOME/taxdump.tar.gz
 # The container can be invoked with extra arguments, in environment
 # variables to avoid polluting the CLI arguments.
 # Reuse boolean argument variables for output file names.
-[ "$KEEP_TAXDUMP" ] && KEEP_TAXDUMP=$HOME/out-taxdump.tar.gz
 [ "$KEEP_TAXDB" ] && KEEP_TAXDB=$HOME/out-taxa.sqlite
-
-# Save the command line since CLI argument scanning will change $@
-INVOCATION=$@
-
-# Evaluate CLI arguments.
-# Note that CLI arguments may be relative to $CAMISIM directory.
-cd $CAMISIM
-eval set -- "$(getopt -o '' -ql ncbi: -- "$@")"
-while [ $# -ge 1 ]; do
-    case "$1" in
-	--ncbi)
-	    # If NCBI taxonomy file specified explicitly, use it for ete2, too.
-	    [ -f "$2" ] ln -s "$2" $TAXDUMP;
-	    shift
-	    ;;
-    esac
-    shift
-done
 
 # Preload ete2 taxonomy, in $HOME, which is writeable, rather than in
 # $CAMISIM, which might not be.
@@ -53,9 +32,8 @@ echo -e "from ete2 import NCBITaxa\nNCBITaxa()" |
     (cd $HOME && python)
 
 # Output directory is $HOME, so can just link taxonomy file.
-[ "$KEEP_TAXDUMP" ] && ln $TAXDUMP $KEEP_TAXDUMP
-[ "$KEEP_TAXDB" ] && ln $HOME/.etetoolkit/taxa.sqlite $KEEP_TAXDB
+[ "$KEEP_TAXDB" ] && ln -f $HOME/.etetoolkit/taxa.sqlite $KEEP_TAXDB
 
 # MAIN EVENT: run the selected program in the expected directory,
 # return exit code.
-cd $CAMISIM && exec python $INVOCATION
+cd $CAMISIM && exec python $@

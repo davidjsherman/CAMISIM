5b79400d5a053d9a1cb5045f78b8e74f38c57f22
David James Sherman <david.sherman@inria.fr>
2020-12-11 11:19:50 +0100

    Improved container image with entrypoint wrapper
    - Works for read-only container or unprivileged user
    - CAMISIM environment variable points to install directory
    - Pin versions scipy, pandas for Python 2.7 specifically
    - Improved ete2 patch to fix sqlite errors
    
    Endpoint works around incorrect assumptions:
    
    - by CAMISIM, which assumes invoked from the install directory
    - by ete2, which assumes RW in invocation and $HOME dirs
    
    Endpoint will preload ete2 taxonomy database, or use an existing
    one if it is provided to the container in $HOME/.etetoolkit/.

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,19 @@ RUN apt update
 RUN apt install -y python2.7 python-pip perl
 RUN perl -MCPAN -e 'install XML::Simple'
 ADD requirements.txt /requirements.txt
+RUN perl -pi -e 'print "scipy==0.16.0\npandas==0.24.1\n" if ($.==2)' /requirements.txt
 RUN cat requirements.txt | xargs -n 1 pip install
-ADD *.py /usr/local/bin/
-ADD scripts /usr/local/bin/scripts
-ADD tools /usr/local/bin/tools
-ADD defaults /usr/local/bin/defaults
-ADD ete2.patch /
-RUN patch $(python -c "import ete2 as _; print(_.__path__[0])")/ncbi_taxonomy/ncbiquery.py ete2.patch
-ENV HOME=/tmp
-WORKDIR /usr/local/bin
-ENTRYPOINT ["python"]
+
+ENV CAMISIM=/usr/local/bin
+
+ADD *.py $CAMISIM/
+ADD scripts $CAMISIM/scripts
+ADD tools $CAMISIM/tools
+ADD defaults $CAMISIM/defaults
+ADD ete2.patch /ete2.patch
+RUN patch $(python -c "import ete2 as _; print(_.__path__[0])")/ncbi_taxonomy/ncbiquery.py /ete2.patch
+
+RUN rm -rf /requirements.txt /ete2.patch /var/lib/{apt,dpkg,cache,log} ~/.cache ~/.cpan 
+
+ADD container_entrypoint.sh $CAMISIM/
+ENTRYPOINT ["sh", "-c", "exec $CAMISIM/container_entrypoint.sh $0 $@"]
---
+++ container_entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Set up an environment for running CAMISIM in a container, even when
+# the container is run read-only (e.g. CWL) and even when the user is
+# unprivileged (e.g. Openshift).
+#
+# Works around two invalid assumptions: by CAMISIM, that it is invoked
+# from its install directory, rather than a writeable working
+# directory; and by ete2, that it can write both in its invocation
+# directory and in $HOME.
+
+echo Container entrypint invocation\ -- "$@"
+
+# CAMISIM expects to be invoked from its install directory.
+# In an ideal world this variable is set by a container image.
+CASISIM=${CAMISIM:-/usr/local/bin}
+
+# Location of the taxonomy database expected or created by ete2.
+ETE2_TAXDB=$HOME/.etetoolkit/taxa.sqlite
+
+# $HOME must be writeable, even if the container user is unprivileged.
+# Note that when invoking from CWL, only the output and temporary
+# directories are guaranteed to be writeable, as per
+# [4.2](https://www.commonwl.org/v1.1/CommandLineTool.html#Runtime_environment)
+touch -c "$HOME/." 2>/dev/null || export HOME=${TMPDIR:=/tmp}
+
+# If the current working directory of this invocation isn't $HOME but does
+# contain an ete2 taxonomy dump, copy the dump to the expected place.
+# (This can happen in containers with unexpected bind mounts.)
+[ -s ./.etetoolkit/taxa.sqlite -a ! ./.etetoolkit/taxa.sqlite -ef $ETE2_TAXDB ] &&
+    mkdir -p $(dirname $ETE2_TAXDB) &&
+    cp ./.etetoolkit/taxa.sqlite $ETE2_TAXDB
+
+# Preload the ete2 taxonomy, in $HOME, which is writeable, rather than in
+# $CAMISIM, which might not be.
+echo -e "from ete2 import NCBITaxa\nNCBITaxa()" |
+    (cd $HOME && python)
+
+# MAIN EVENT: run the selected program in the expected directory,
+# return exit code.
+cd $CAMISIM && exec python "$@"

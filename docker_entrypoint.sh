5cb535381356a9a1de98fb703749191e37a69f0d
David James Sherman <david.sherman@inria.fr>
2020-12-15 16:14:02 +0100

    Taxonomy DBs must be copied

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -13,6 +13,9 @@
 # In an ideal world this variable is set by the container image.
 CASISIM=${CAMISIM:-/usr/local/bin}
 
+# Location of the taxonomy database created by ete2.
+ETE2_TAXDB=$HOME/.etetoolkit/taxa.sqlite
+
 # $HOME must be writeable, even if the container user is unprivileged.
 # Note as per [4.2](https://www.commonwl.org/v1.1/CommandLineTool.html#Runtime_environment)
 # only the output and temporary directories need be writeable.
@@ -20,17 +23,15 @@ touch "$HOME/." || export HOME=${TMPDIR:=/tmp}
 
 # The container can be invoked with extra arguments, in environment
 # variables to avoid polluting the CLI arguments.
-[ -s "$TAXDB" ] && ln -s "$TAXDB" $HOME/.etetoolkit/taxa.sqlite
-# Reuse boolean argument variables for output file names.
-[ "$KEEP_TAXDB" ] && KEEP_TAXDB=$HOME/out-taxa.sqlite
+[ -s "$TAXDB" -a ! -s $ETE2_TAXDB ] && cp "$TAXDB" $ETE2_TAXDB
 
 # Preload ete2 taxonomy, in $HOME, which is writeable, rather than in
 # $CAMISIM, which might not be.
 echo -e "from ete2 import NCBITaxa\nNCBITaxa()" |
     (cd $HOME && python)
 
-# Output directory is $HOME, so can just link taxonomy file.
-[ "$KEEP_TAXDB" ] && ln -f $HOME/.etetoolkit/taxa.sqlite $KEEP_TAXDB
+# Copy taxonomy file to output if requested.
+[ "$KEEP_TAXDB" ] && cp $ETE2_TAXDB $HOME/out-taxa.sqlite
 
 # MAIN EVENT: run the selected program in the expected directory,
 # return exit code.
